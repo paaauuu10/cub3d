9887e3013a96cc5a3dcad2ec18bc30d638ced015
Pau Botargues Julio <pbotargu@car6s3.42barcelona.com>
2024-08-23 11:46:51 +0200

    <starting>

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,15 +6,15 @@
 /*   By: pborrull <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 11:58:47 by pborrull          #+#    #+#             */
-/*   Updated: 2024/08/22 10:33:53 by pborrull         ###   ########.fr       */
+/*   Updated: 2024/08/22 13:11:47 by pborrull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "minilibx-linux/mlx.h"
-//# include "libft/libft.h"
+# include "libx/mlx.h"
+# include "libft/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
